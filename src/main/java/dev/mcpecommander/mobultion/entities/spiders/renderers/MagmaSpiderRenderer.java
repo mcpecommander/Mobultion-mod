03f9cc3da74c30cf539b433e54e1d992b10c6b29
mcpecommander <hosam201520152015@gmail.com>
2021-06-19 09:51:39 +0200

    Fixed the entity rotating after death

--- src/main/java/dev/mcpecommander/mobultion/entities/spiders/renderers/MagmaSpiderRenderer.java
+++ src/main/java/dev/mcpecommander/mobultion/entities/spiders/renderers/MagmaSpiderRenderer.java
@@ -1,5 +1,6 @@
 package dev.mcpecommander.mobultion.entities.spiders.renderers;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import dev.mcpecommander.mobultion.entities.spiders.entities.MagmaSpiderEntity;
 import dev.mcpecommander.mobultion.entities.spiders.layers.SpiderEyesLayer;
 import dev.mcpecommander.mobultion.entities.spiders.layers.SpiderMagmaLayer;
@@ -14,6 +15,12 @@ public class MagmaSpiderRenderer extends GeoEntityRenderer<MagmaSpiderEntity> {
         super(renderManager, new MagmaSpiderModel());
         this.shadowRadius = 0.7F;
         this.addLayer(new SpiderMagmaLayer(this));
-        this.addLayer(new SpiderEyesLayer(this, "magmaspider"));
+        this.addLayer(new SpiderEyesLayer<>(this, "magmaspider"));
+    }
+
+    @Override
+    protected void applyRotations(MagmaSpiderEntity entityLiving, MatrixStack matrixStackIn, float ageInTicks, float rotationYaw, float partialTicks) {
+        if(entityLiving.isDeadOrDying()) return;
+        super.applyRotations(entityLiving, matrixStackIn, ageInTicks, rotationYaw, partialTicks);
     }
 }
