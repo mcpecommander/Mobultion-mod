41e4878749658906161fc91f1353d808de88d33d
mcpecommander <hosam201520152015@gmail.com>
2021-06-25 09:22:31 +0200

    Adjusted the timing and made it sync across different players

--- src/main/java/dev/mcpecommander/mobultion/items/ThunderStaffItem.java
+++ src/main/java/dev/mcpecommander/mobultion/items/ThunderStaffItem.java
@@ -18,7 +18,8 @@ import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
-import software.bernie.geckolib3.core.AnimationState;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.fml.network.PacketDistributor;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -26,15 +27,18 @@ import software.bernie.geckolib3.core.controller.AnimationController;
 import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.network.GeckoLibNetwork;
+import software.bernie.geckolib3.network.ISyncable;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
 /* McpeCommander created on 23/06/2021 inside the package - dev.mcpecommander.mobultion.items */
-public class ThunderStaffItem extends Item implements IAnimatable {
+public class ThunderStaffItem extends Item implements IAnimatable, ISyncable {
 
     public AnimationFactory factory = new AnimationFactory(this);
 
     public ThunderStaffItem() {
         super(new Properties().stacksTo(1).tab(ModSetup.ITEM_GROUP).setNoRepair().setISTER(() -> ThunderStaffRenderer::new));
+        GeckoLibNetwork.registerSyncable(this);
     }
 
     @Override
@@ -52,14 +56,22 @@ public class ThunderStaffItem extends Item implements IAnimatable {
         return 200;
     }
 
+    @Override
+    public void releaseUsing(ItemStack itemStack, World world, LivingEntity holder, int charge) {
+        if(getUseDuration(itemStack) - charge < 40 && !world.isClientSide){
+            final int id = GeckoLibUtil.guaranteeIDForStack(itemStack, (ServerWorld) world);
+            final PacketDistributor.PacketTarget target = PacketDistributor.TRACKING_ENTITY_AND_SELF
+                    .with(() -> holder);
+            GeckoLibNetwork.syncAnimation(target, this, id, 1);
+        }
+    }
 
     @Override
     public void onUseTick(World world, LivingEntity holder, ItemStack itemStack, int charge) {
-        if(getUseDuration(itemStack) - charge > 30 && holder instanceof PlayerEntity){
+        if(getUseDuration(itemStack) - charge > 35 && holder instanceof PlayerEntity){
             if(!world.isClientSide){
                 holder.stopUsingItem();
                 RayTraceResult result = getPOVHitResult(world, (PlayerEntity) holder, RayTraceContext.FluidMode.ANY);
-                System.out.println(result.getType());
                 if(result.getType() == RayTraceResult.Type.MISS) return;
                 LightningBoltEntity entity = new LightningBoltEntity(EntityType.LIGHTNING_BOLT, world);
                 if(result instanceof BlockRayTraceResult){
@@ -72,7 +84,7 @@ public class ThunderStaffItem extends Item implements IAnimatable {
                 }
 
                 world.addFreshEntity(entity);
-                ((PlayerEntity) holder).getCooldowns().addCooldown(this, 400);
+                ((PlayerEntity) holder).getCooldowns().addCooldown(this, 2);
             }
 
 
@@ -95,21 +107,27 @@ public class ThunderStaffItem extends Item implements IAnimatable {
                 player.getBoundingBox().expandTowards(temp.x * 60, temp.y * 60, temp.z * 60)
                         .inflate( 2,  2,  2),
                 entity -> entity != null && !entity.isSpectator());
+        BlockRayTraceResult blockRayTraceResult = world.clip(new RayTraceContext(from, to, RayTraceContext.BlockMode.OUTLINE, fluidMode, player));
         if(entityRayTraceResult == null) {
-            return world.clip(new RayTraceContext(from, to, RayTraceContext.BlockMode.OUTLINE, fluidMode, player));
-        }else{
-            return entityRayTraceResult;
+            return blockRayTraceResult;
+        }else if (player.distanceToSqr(blockRayTraceResult.getBlockPos().getX(),
+                blockRayTraceResult.getBlockPos().getY(),
+                blockRayTraceResult.getBlockPos().getZ()) < player.distanceToSqr(entityRayTraceResult.getEntity())) {
+            return blockRayTraceResult;
         }
+        return entityRayTraceResult;
     }
 
     @Override
     public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
         ItemStack itemstack = player.getItemInHand(hand);
+        if(hand == Hand.OFF_HAND) return ActionResult.fail(itemstack);
         player.startUsingItem(hand);
-        if(world.isClientSide) {
-            AnimationController controller = GeckoLibUtil.getControllerForStack(this.factory, itemstack, "controller");
-            if(controller.getAnimationState() == AnimationState.Stopped) controller.clearAnimationCache();
-            controller.setAnimation(new AnimationBuilder().addAnimation("light", false));
+        if(!world.isClientSide) {
+            final int id = GeckoLibUtil.guaranteeIDForStack(itemstack, (ServerWorld) world);
+            final PacketDistributor.PacketTarget target = PacketDistributor.TRACKING_ENTITY_AND_SELF
+                    .with(() -> player);
+            GeckoLibNetwork.syncAnimation(target, this, id, 0);
         }
         return ActionResult.consume(itemstack);
     }
@@ -121,5 +139,18 @@ public class ThunderStaffItem extends Item implements IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return this.factory;
+    }
+
+    @Override
+    public void onAnimationSync(int id, int state) {
+        final AnimationController<?> controller = GeckoLibUtil.getControllerForID(this.factory, id, "controller");
+        if (state == 0) {
+            controller.markNeedsReload();
+            controller.clearAnimationCache();
+            controller.setAnimation(new AnimationBuilder().addAnimation("light", false));
+        }else if (state == 1){
+            controller.markNeedsReload();
+            controller.setAnimation(new AnimationBuilder().addAnimation("reset", false));
+        }
     }
 }
