75cf5de9b8cd9fccc38ca3dab6eaf59776bbe91e
mcpecommander <hosam201520152015@gmail.com>
2021-07-02 21:17:42 +0200

    Changed the entire rendering method to stop it from breaking item rendering

--- src/main/java/dev/mcpecommander/mobultion/entities/endermen/layers/EndermanEyesLayer.java
+++ src/main/java/dev/mcpecommander/mobultion/entities/endermen/layers/EndermanEyesLayer.java
@@ -1,39 +1,63 @@
 package dev.mcpecommander.mobultion.entities.endermen.layers;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import dev.mcpecommander.mobultion.entities.endermen.entities.WanderingEndermanEntity;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import dev.mcpecommander.mobultion.entities.endermen.entities.MobultionEndermanEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.renderers.geo.GeoLayerRenderer;
 import software.bernie.geckolib3.renderers.geo.IGeoRenderer;
+import software.bernie.geckolib3.util.RenderUtils;
 
 import static dev.mcpecommander.mobultion.Mobultion.MODID;
 
 /* McpeCommander created on 25/06/2021 inside the package - dev.mcpecommander.mobultion.entities.endermen.layers */
-public class EndermanEyesLayer extends GeoLayerRenderer<WanderingEndermanEntity> {
+public class EndermanEyesLayer<T extends MobultionEndermanEntity> extends GeoLayerRenderer<T> {
 
     /**
      * The resource location for a texture that matches the model texture in size but highlights the parts that will
      * light.
      */
-    private static final ResourceLocation ENDERMAN_EYES = new ResourceLocation(MODID, "textures/entity/endermen/wanderingendermaneyes.png");
+    private final ResourceLocation ENDERMAN_EYES;
     /**
      * The resource location for the geckolib model of the entity that has this layer.
      */
-    private static final ResourceLocation ENDERMAN_MODEL = new ResourceLocation(MODID, "geo/endermen/wanderingenderman.json");
-    IGeoRenderer<WanderingEndermanEntity> renderer;
+    private final ResourceLocation ENDERMAN_MODEL;
+    IGeoRenderer<T> renderer;
 
-    public EndermanEyesLayer(IGeoRenderer entityRendererIn) {
+    public EndermanEyesLayer(IGeoRenderer<T> entityRendererIn, String endermanName) {
         super(entityRendererIn);
         this.renderer = entityRendererIn;
-
+        ENDERMAN_MODEL = new ResourceLocation(MODID, "geo/endermen/"+ endermanName +".json");
+        String eyes = endermanName.equals("wanderingenderman") ? "wanderingenderman" : "enderman";
+        ENDERMAN_EYES = new ResourceLocation(MODID, "textures/entity/endermen/" + eyes + "eyes.png");
     }
 
     @Override
-    public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, WanderingEndermanEntity entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
-        RenderType eyes = RenderType.eyes(ENDERMAN_EYES);
-        this.renderer.render(this.getEntityModel().getModel(ENDERMAN_MODEL), entitylivingbaseIn, partialTicks, eyes, matrixStackIn, bufferIn, bufferIn.getBuffer(eyes), packedLightIn, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1f);
+    public void render(MatrixStack stack, IRenderTypeBuffer bufferIn, int packedLightIn, T entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
+
+        stack.pushPose();
+        //Copied from GeoLayerRenderer.
+        IVertexBuilder ivertexbuilder = bufferIn.getBuffer(RenderType.eyes(ENDERMAN_EYES));
+        //The main group of my model is "all"
+        //This section is from renderRecursively but I am doing the first step here to stop anything other than the
+        //head from rendering which guarantees that the render type is not changed when rendering items.
+        GeoBone all = getEntityModel().getModel(ENDERMAN_MODEL).topLevelBones.get(0);
+        RenderUtils.translate(all, stack);
+        RenderUtils.moveToPivot(all, stack);
+        RenderUtils.rotate(all, stack);
+        RenderUtils.scale(all, stack);
+        RenderUtils.moveBackFromPivot(all, stack);
+        for (GeoBone childBone : all.childBones) {
+            //Makes sure that only the head main group is being rendered since that the only part that lights in the
+            //eye layer.
+            if(!childBone.getName().equals("Head")) continue;
+            this.renderer.renderRecursively(childBone, stack, ivertexbuilder, packedLightIn,
+                    LivingRenderer.getOverlayCoords(entitylivingbaseIn, 0.0F), 1f, 1f, 1f, 1f);
+        }
+        stack.popPose();
     }
 }
