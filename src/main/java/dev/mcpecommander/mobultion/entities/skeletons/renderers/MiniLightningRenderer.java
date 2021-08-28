76cf31f4a01feeaf90a7aa1859dc47cf688497f0
mcpecommander <hosam201520152015@gmail.com>
2021-08-28 20:06:14 +0200

    Added documentation

--- src/main/java/dev/mcpecommander/mobultion/entities/skeletons/renderers/MiniLightningRenderer.java
+++ src/main/java/dev/mcpecommander/mobultion/entities/skeletons/renderers/MiniLightningRenderer.java
@@ -22,21 +22,33 @@ public class MiniLightningRenderer extends EntityRenderer<MiniLightningEntity> {
         super(rendererManager);
     }
 
+    //TODO: should probably fix this but the entity get rendered so rarely that it shouldn't be a problem for now.
     @Override
     public boolean shouldRender(@Nonnull MiniLightningEntity entity, @Nonnull ClippingHelper clippingHelper, double x,
                                 double y, double z) {
         return true;
     }
 
+    /**
+     * Gets called every client tick which can be up to the fps setting.
+     * @param entity The entity being rendered.
+     * @param entityYaw The rotation yaw of the entity.
+     * @param partialTicks The partial ticks used to interpolate the 20 server ticks into the fps-based client ticks.
+     * @param matrixStack The matrix stack instance used for poses, rotations and translations.
+     * @param renderTypeBuffer The render buffer
+     * @param packedLight The light level using Minecraft magic light numbers.
+     */
     @Override
-    public void render(@Nonnull MiniLightningEntity entity, float p_225623_2_, float p_225623_3_,
-                       @Nonnull MatrixStack matrixStack, @Nonnull IRenderTypeBuffer renderTypeBuffer, int p_225623_6_) {
+    public void render(@Nonnull MiniLightningEntity entity, float entityYaw, float partialTicks,
+                       @Nonnull MatrixStack matrixStack, @Nonnull IRenderTypeBuffer renderTypeBuffer, int packedLight) {
 
         matrixStack.pushPose();
+        //Rotate the whole thing constantly.
         matrixStack.mulPose(Vector3f.YP.rotation(entity.tickCount * 0.2f));
+        //Set the render type to vanilla lightning and get the vertex builder.
         IVertexBuilder ivertexbuilder = renderTypeBuffer.getBuffer(RenderType.lightning());
         Matrix4f matrix4f = matrixStack.last().pose();
-
+        //Render a quad and then rotate by 30 degrees and repeat until a spiky circle is formed.
         for(int i = 0; i < 12; i++){
             quad(matrix4f, ivertexbuilder, entity.tickCount/20f);
             matrixStack.mulPose(Vector3f.YP.rotationDegrees(30));
