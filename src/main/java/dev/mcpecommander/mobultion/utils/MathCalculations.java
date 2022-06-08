1d138a9e8cc5c05037a441c4144cedb7a698d87d
mcpecommander <hosam201520152015@gmail.com>
2022-06-08 07:56:20 +0200

    Added looking at method

--- src/main/java/dev/mcpecommander/mobultion/utils/MathCalculations.java
+++ src/main/java/dev/mcpecommander/mobultion/utils/MathCalculations.java
@@ -1,6 +1,9 @@
 package dev.mcpecommander.mobultion.utils;
 
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.phys.Vec3;
 
 /* McpeCommander created on 09/07/2021 inside the package - dev.mcpecommander.mobultion.utils */
 public class MathCalculations {
@@ -13,6 +16,33 @@ public class MathCalculations {
             return Mth.clamp(value, finalStart, finalStop);
         } else {
             return Mth.clamp(value, finalStop, finalStart);
+        }
+    }
+
+    public static void rotateTowardsEntity(Entity lookingEntity, Entity lookingTarget,  float rotationSpeed) {
+        Vec3 distanceVector = lookingTarget.position().subtract(lookingEntity.position());
+        if (distanceVector.lengthSqr() != 0.0D) {
+            lookingEntity.setYRot((float)(Mth.atan2(distanceVector.z, distanceVector.x) * (double)(180F / (float)Math.PI)) + 90.0F);
+            lookingEntity.setXRot((float)(Mth.atan2(distanceVector.horizontalDistance(), distanceVector.y) * (double)(180F / (float)Math.PI)) - 90.0F);
+
+            while(lookingEntity.getXRot() - lookingEntity.xRotO < -180.0F) {
+                lookingEntity.xRotO -= 360.0F;
+            }
+
+            while(lookingEntity.getXRot() - lookingEntity.xRotO >= 180.0F) {
+                lookingEntity.xRotO += 360.0F;
+            }
+
+            while(lookingEntity.getYRot() - lookingEntity.yRotO < -180.0F) {
+                lookingEntity.yRotO -= 360.0F;
+            }
+
+            while(lookingEntity.getYRot() - lookingEntity.yRotO >= 180.0F) {
+                lookingEntity.yRotO += 360.0F;
+            }
+
+            lookingEntity.setXRot(Mth.lerp(rotationSpeed, lookingEntity.xRotO, lookingEntity.getXRot()));
+            lookingEntity.setYRot(Mth.lerp(rotationSpeed, lookingEntity.yRotO, lookingEntity.getYRot()));
         }
     }
 }
